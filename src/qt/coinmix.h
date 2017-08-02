001d30d15cf77b18220101e8b97df230dde98418
Christian Knöpke <apfelbaum@email.de>
2017-08-02 07:30:50 +0200

    Change Define

--- src/qt/coinmix.h
+++ src/qt/coinmix.h
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 //
 #ifndef BITCOIN_QT_COINMIX_H
-#define BITCOIN_QT__COINMIX_H
+#define BITCOIN_QT_COINMIX_H
 
 #include "amount.h"
 

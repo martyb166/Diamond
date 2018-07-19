454fd4721ad9b8a240dd69f1dfc5aa410cd1d440
Chris <apfelbaum@email.de>
2018-07-19 14:01:13 +0200

    Version 3.0.1.2.
    Set down Bantime, change min protocol version.

--- src/net.cpp
+++ src/net.cpp
@@ -505,7 +505,7 @@ bool CNode::IsBanned(CNetAddr ip)
 
 bool CNode::Ban(const CNetAddr& addr)
 {
-    int64_t banTime = GetTime() + GetArg("-bantime", 60 * 60 * 24); // Default 24-hour ban
+    int64_t banTime = GetTime() + GetArg("-bantime", 60 * 60 * 3); // Default 24-hour ban
     {
         LOCK(cs_setBanned);
         if (setBanned[addr] < banTime)
